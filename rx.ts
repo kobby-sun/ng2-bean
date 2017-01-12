668dd1badd7209eff093e2af7d82cd890ec33b1a
Kobby Sun <ksun@geomatic.com.au>
2017-01-12 16:35:41 +1100

    rewrite polling func

--- rx.ts
+++ rx.ts
@@ -1,38 +1,62 @@
-declare var Rx: any;
+import { Response } from '@angular/http';
+import { Router, NavigationStart } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Subscriber } from 'rxjs/Subscriber';
 
-export function polling(func, before, tick, freq = 10000) {
-        return Rx.Observable.create(function (observer) {
+export function polling(func, before: Function, router: Router, tick: Function, freq: number = 10000): Subscriber<Response> {
+  let req: Function
+  let pass: Function
+  return Observable.create((observer: Subscriber<Response>) => {
+    req = function () {
+      if (before) before();
+      var subscription = func().subscribe(result => {
 
-                function requestData() {
-                        if (before) before();
-                        var subscription = func().subscribe(result => {
+        observer.next(result);
+        subscription.unsubscribe();
+      }, err => {
+        observer.next(err);
+        subscription.unsubscribe();
+      }, () => {
+        subscription.unsubscribe();
+      });
+    }
 
-                                observer.onNext(result);
-                                subscription.unsubscribe();
-                        }, err => {
-                                observer.onNext(err);
-                                subscription.unsubscribe();
-                        }, () => {
-                                subscription.unsubscribe();
-                        });
-                }
+    pass = function () {
+      observer.next(null);
+    }
 
-                requestData()
-                var timerId = setInterval(requestData, freq);
+    req()
+    // var timerId = setInterval(req, freq);
 
-                return function () {
-                        observer.onCompleted();
-                        clearInterval(timerId);
-                }
-        }).subscribe(
-                function (x) {
-                        tick(x)
-                        // console.log('Next: ', x);
-                },
-                function (err) {
-                        console.log('Error: ', err);
-                },
-                function () {
-                        // console.log('Completed');
-                });;
+    let routerSub = router == null ? null : router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        //   console.log('> router changed, observer complete')
+        // console.log('observer.closed', observer.closed)
+        observer.complete();
+      }
+    })
+
+    return function () {
+      // console.log('> observer complete')
+      // console.log('observer.closed', observer.closed)
+      if (routerSub != null)
+        routerSub.unsubscribe()
+      if (!observer.closed)
+        observer.complete();
+      // clearInterval(timerId);
+    }
+  }).subscribe(
+    function (x) {
+      if (tick(x))
+        setTimeout(req, freq)
+      else
+        setTimeout(pass, freq)
+      // console.log('Next: ', x);
+    },
+    function (err) {
+      console.log('Error: ', err);
+    },
+    function () {
+      // console.log('Completed');
+    });
 }
